547ceaa1f8e6b2a4447ea488544c803ce633fb71
HP <HP@LAPTOP-316GIRT1>
2025-10-27 20:39:02 +0700

    Pratikum 5 Kelas A

---
+++ 2025_prakalpro_A_251532007/src/pekan5/TugasPekan5.java
@@ -0,0 +1,20 @@
+package pekan5;
+
+public class TugasPekan5 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		for(int i = 1; i <= n; i++) {
+			for (int j = 1; j <= (n-i); j++) {
+				System.out.print(".");
+			}
+			System.out.print(i);
+			for(int j=1; j<= (i-1); j++) {
+			   System.out.print(".");
+			}
+			System.out.println();
+	   }
+	}
+}
+
+
